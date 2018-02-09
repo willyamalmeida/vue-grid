ae282eb048240d1d35a498b0f63b188667d4fb49
Willyam Santana Almeida <willyamalmeida@hotmail.com>
2018-02-09 15:55:10 -0200

    add test with jest vue

--- test/parserLink.test.js
+++ test/parserLink.test.js
@@ -2,13 +2,17 @@ import parserLink from "./../src/utils/parserLink";
 
 const mockLink = '<http://localhost:3000/rows?_page=1&_limit=10>; rel="first", <http://localhost:3000/rows?_page=2&_limit=10>; rel="next", <http://localhost:3000/rows?_page=10&_limit=10>; rel="last"';
 
-test("mock link parsed from parser function", () => {
-    let result = parserLink(mockLink, "_page", "_limit");
-    let expected = {
-        first: { page: 1, perPage: 10 },
-        next: { page: 2, perPage: 10 },
-        last: { page: 10, perPage: 10 }
-    };
+describe("parserLink.js", () => {
 
-    expect(result).toEqual(expected);
+    it("mock link parsed from parser function", () => {
+        let result = parserLink(mockLink, "_page", "_limit");
+        let expected = {
+            first: { page: 1, perPage: 10 },
+            next: { page: 2, perPage: 10 },
+            last: { page: 10, perPage: 10 }
+        };
+
+        expect(result).toEqual(expected);
+    });
 });
+
--- test/specs/VueFilter.spec.js
+++ test/specs/VueFilter.spec.js
@@ -1,18 +1,63 @@
-import Vue from "vue";
 import { mount } from "vue-test-utils";
 import VueFilter from "./../../src/components/VueFilter.vue";
 
+const createVueFiler = function(propValue){
+    return mount(VueFilter, {
+        propsData: {
+            value: propValue
+        }
+    });
+}
+
 describe("VueFilter", () => {
-    it("has a methods hook", () => {
+    it("test has a methods hook", () => {
         expect(typeof VueFilter.methods).toBe("object");
     });
 
-    it("name of component", () => {
+    it("test name of component", () => {
         expect(VueFilter.name).toBe("vue-filter");
     });
 
-    it("is a Vue component", () => {
-        const wrapper = mount(VueFilter);
-        expect(wrapper.isVueInstance()).toBeTruthy();
+    it("test is a Vue component", () => {
+        const vueFilter = mount(VueFilter);
+        expect(vueFilter.isVueInstance()).toBeTruthy();
+    });
+
+    it("test props value", () => {
+        const wrapper = createVueFiler("my filter");
+        let value = wrapper.props().value;
+
+        expect(value).toBe("my filter");
+    });
+
+
+    it("test update input value", () => {
+        const expected = "new filter";
+        const propValue = "my filter";
+        const wrapper = createVueFiler(propValue);
+
+        let input = wrapper.find("input");
+
+        expect(input.element.value).toBe(propValue);
+
+        input.element.value = expected;
+        input.trigger("input");
+
+        let emitted = wrapper.emitted("input");
+
+        expect(emitted).toBeTruthy();
+        expect(emitted.length).toBe(1);
+
+        //emitted[0] = ["value"]
+        //emitted[0][0] = "value"
+        let value = emitted[0][0];
+
+        expect(value).toBe(expected);
+    });
+
+    it("test VueFilter html structure", () => {
+        const wrapper = createVueFiler("my filter");
+
+        expect(wrapper.element).toMatchSnapshot();
     });
 });
